fe5627d887be617380f59e8ba2fc661357b8e235
Otavio Tonello <otaviotonello0102@gmail.com>
2021-11-17 18:00:57 -0300

    Update

--- src/ProjetoZoo/ProgramaPrincipal.java
+++ src/ProjetoZoo/ProgramaPrincipal.java
@@ -10,10 +10,22 @@ public class ProgramaPrincipal {
 		
 		String entrada;
 		Scanner in = new Scanner(System.in);
-		System.out.println("Digite o nome de um Animal: ");
+		
+		//COLLECTIONS
+		List<String> listaAnimais = new ArrayList<>();
+		listaAnimais.add("Tatu-Canastra");
+		listaAnimais.add("Tamandua-Bandeira");
+		
+		////////////////////////////ENTRADA DO PROGRAMA//////////////////////////////////////
+		
+		System.out.println("Olá! Seja muito Bem Vindo(a) ao Projeto Zoo :)");
+		System.out.println("A nossa missão é ser um canal de informação sobre o mundo animal!");
+		System.out.println("\nAqui está nossa lista de animais disponíveis para consulta: ");
+		System.out.println(listaAnimais);
+		System.out.println("\nDigite o nome de um animal disponível: ");
 		entrada = in.next();
 	
-		////////////////////////////////////////////////
+		//////////////////////////ESTRUTURA CENTRAL DO PROGRAMA//////////////////////////////
 		
 		//MAMIFEROS
 
@@ -29,39 +41,33 @@ public class ProgramaPrincipal {
 		tb.setTamanhoMax(110);
 		tb.setComida("Formiga");
 		
-		
 		//TATU
 		
 		TatuCanastra tc = new TatuCanastra();
 		
 		tc.setNome("Tatu-Canastra");
 		tc.setExpcVida(15);
-		tc.setHabitat("terrestre");
+		tc.setHabitat("Terrestre");
 		tc.setExtincao(true);
 		tc.setPesoMax(33);
 		tc.setTamanhoMax(100);
-		tc.setComida("Aranhas e escorpiões");
+		tc.setComida("Aranhas e Escorpiões");
 		
 		//////////////////////////////////////////////
-		
-		if(entrada.equalsIgnoreCase("tamandua")) {
-			System.out.println("Nome: " + tb.getNome() + " | Tamanho: " + tb.getTamanhoMax() +
-			"\nExpc. Vida: " + tb.getExpcVida() + " | Peso Max: " + tb.getNome() + 
-			"\nAmeaçado de Ex: " + tb.isExtincao() + " | Habitat: " + tb.getHabitat() + " | Comida: " +
+		if(entrada.equalsIgnoreCase("tamandua") || entrada.equalsIgnoreCase("tamandua bandeira")) {
+			System.out.println("\n--------------------------------------------------------------------------------");
+			System.out.println("\n| Nome: " + tb.getNome() + " \n| Tamanho: " + tb.getTamanhoMax() +
+			"\n| Expectativa de Vida: " + tb.getExpcVida() + " \n| Peso Máximo: " + tb.getNome() + 
+			"\n| Ameaçado de Extinação? - " + tb.isExtincao() + " \n| Habitat Natural: " + tb.getHabitat() + " \n| Alimento Predileto: " +
 			tb.getComida());
-		} else if (entrada.equalsIgnoreCase("tatu")) {
-			System.out.println("Nome: " + tc.getNome() + " | Tamanho: " + tc.getTamanhoMax() +
-			"\nExpc. Vida: " + tc.getExpcVida() + " | Peso Max: " + tc.getNome() + 
-			"\nAmeaçado de Ex: " + tc.isExtincao() + " | Habitat: " + tc.getHabitat() + " | Comida: " +
+			} else if (entrada.equalsIgnoreCase("tatu") || entrada.equalsIgnoreCase("tatu canastra") ) {
+			System.out.println("\n--------------------------------------------------------------------------------");
+			System.out.println("\n| Nome: " + tc.getNome() + " \n| Tamanho: " + tc.getTamanhoMax() +
+			"\n| Expectativa de Vida: " + tc.getExpcVida() + " \n| Peso Máximo: " + tc.getNome() + 
+			"\n| Ameaçado de Extinação? - " + tc.isExtincao() + " \n| Habitat Natural: " + tc.getHabitat() + " \n| Alimento Predileto: " +
 			tc.getComida());
-		}
-		
-		
-		//COLLECTIONS
-		List<String> listaAnimais = new ArrayList<>();
-		listaAnimais.add("Tatu-Canastra");
-		listaAnimais.add("Tamandua-Bandeira");
-		System.out.println(listaAnimais);
+			}
+		/////////////////////////////////////////////
 		
 		//OPTIONS
 		
